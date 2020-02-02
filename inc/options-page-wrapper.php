d59817d5f84e90ee0e26be36109f79d5ddb3baf0
josegallo <josegallo@gmail.com>
2020-02-02 17:05:34 +0100

    initial: basic back-end

---
+++ inc/options-page-wrapper.php
@@ -0,0 +1,152 @@
+<div class="wrap">
+
+	<div id="icon-options-general" class="icon32"></div>
+	<h1>NY Times Articles</h1>
+
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-2">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+				<div class="meta-box-sortables ui-sortable">
+					<?php if (!isset($jgnyt_search) || $jgnyt_search == "" ): ?>
+					<!-- BOX:form API KEY -->
+					<div class="postbox">
+
+						<button type="button" class="handlediv" aria-expanded="true" >
+							<span class="screen-reader-text">Toggle panel</span>
+							<span class="toggle-indicator" aria-hidden="true"></span>
+						</button>
+						<!-- Toggle -->
+
+						<h2 class="hndle"><span>Let's get started</span>
+						</h2>
+
+						<div class="inside">
+							<form method = "post" action="">
+							<input type="hidden" name = "jgnyt_form_submited" value = "Y"></input>
+								<table class="form-table">
+									<tr valign="top">
+										<td scope="row"><label for="tablecell">Search String</label></td>
+										<td><input name = "jgnyt_search" id = "jgnyt_search" type="text" value="" class="regular-text"/></td>
+									</tr>
+									<tr valign="top">
+										<td scope="row"><label for="tablecell">API Key </label></td>
+										<td><input name = "jgnyt_apikey" id = "jgnyt_apikey" type="text" value="" class="regular-text"/></td>
+									</tr>
+								</table>
+								<p>
+									<input class="button-primary" type="submit" name="jgnyt_form_submit" value="Save" />
+								</p>
+							</form>
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<?php else: ?>
+					<!-- .postbox -->
+					<!-- BOX:ARTICLES -->
+					<div class="postbox">
+
+						<button type="button" class="handlediv" aria-expanded="true" >
+							<span class="screen-reader-text">Toggle panel</span>
+							<span class="toggle-indicator" aria-hidden="true"></span>
+						</button>
+						<!-- Toggle -->
+
+						<h2 class="hndle"><span>Let's get started</span>
+						</h2>
+
+						<div class="inside">
+							
+							<p>Below are the 10 articles. <br> 
+							</p>
+
+								<ul class="pgnyt-articles">
+									<?php for( $i = 0; $i < 10; $i++ ):?>									
+										<li>
+											<ul>
+												<li>
+													<!-- //https://ionic4test.enoturismo-reservas.com/wp-content/uploads/2020/01/pg_dummy.jpg// -->
+													<img width="120px" src="https://ionic4test.enoturismo-reservas.com/wp-content/uploads/2020/01/pg_dummy.jpg ">				
+												</li>
+
+												<li class="pgnyt-articles-name">
+													<a href="#">
+														Article Text and Link
+														
+													</a>
+												</li>
+
+												<li class="pgnyt-articles-paragraph">
+													<p>First Line of Content</p>
+												</li>
+
+											</ul>									
+										</li>								
+									<?php endfor; ?>
+								</ul>
+						</div>
+						
+						<!-- .inside -->
+
+					</div>
+					
+					<?php endif ?>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
+
+			</div>
+			<!-- post-body-content -->
+
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
+
+				<div class="meta-box-sortables">
+
+					<div class="postbox">
+
+						<button type="button" class="handlediv" aria-expanded="true" >
+							<span class="screen-reader-text">Toggle panel</span>
+							<span class="toggle-indicator" aria-hidden="true"></span>
+						</button>
+						<!-- Toggle -->
+
+						<h2 class="hndle"><span>Settings</span></h2>
+
+						<div class="inside">
+							<form method = "post" action="">
+								<p>
+									<input name = "jgnyt_search" id = "jgnyt_search" type="text" value="Search String" class="all-options"/>
+									<input name = "jgnyt_apikey" id = "jgnyt_apikey" type="text" value="APIKey" class="all-options"/>
+								</p>
+								<p>
+									<input class="button-primary" type="submit" name="jgnyt_form_submit" value="Save" />
+								</p>
+							</form>
+							
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables -->
+
+			</div>
+			<!-- #postbox-container-1 .postbox-container -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <!-- .wrap -->
+
---
+++ jgnyt-articles.php
@@ -0,0 +1,71 @@
+<?php 
+
+/*
+*
+* Plugin name: Jose Gallo get articles from New Yor Times
+* Plguin URI: http//josegallo.net
+* Description: Provides both widgets and shortcodes to help you display NY Times articles on your wordpress website.
+* Version: 1.0
+* Autor: Jose Gallo
+* Author URI: http//josegallo.net
+* License: GPL2
+*
+*/
+
+
+//create global variable for url plugin
+$plugin_url = WP_PLUGIN_URL . '/jgnty-articles';
+
+//create page on the admin page. Section Settings
+
+function jgnyt_articles_menu()
+{
+	add_options_page( // function to add page on admin menu setting
+			'Jose Gallo NY Timmes Articles Plugin', //name of the page
+			'NY Times Articles', // Menu Title
+			'manage_options', //Capability
+			'jgnyt-articles', //slug
+			'jgnyt_articles_options_page' //function to run
+		); //add submenu page to the Setting menu
+}
+
+add_action('admin_menu', 'jgnyt_articles_menu'); //hook function jgnyt_articles_menu to admin_menu
+
+
+//add back-end dashboard (file ) options-page-wrapper.php
+function jgnyt_articles_options_page (){
+	//check if user has permissions
+	if (!current_user_can('manage_options')) { //if user can not manage options
+		wp_die("You don't have enough permisions"); //throw eror
+	}
+
+	// get the post value from APIKEY and String form at dashobaord
+	if (isset($_POST['jgnyt_form_submited'])) {
+		$hidden_field = esc_html($_POST['jgnyt_form_submited'] ) ;
+		
+		if ($hidden_field =="Y"){
+			$jgnyt_search = esc_html( $_POST["jgnyt_search"] );
+			$jgnyt_apikey = esc_html( $_POST["jgnyt_apikey"] );
+			echo $jgnyt_search . $jgnyt_apikey;
+		}
+		
+	} 
+	
+	global $plugin_url;
+	//instert blocks of the section
+	require('inc/options-page-wrapper.php');
+}
+
+//add stytle to plugin
+function jgnyt_articles_backend_styles(){
+	
+	wp_enqueue_style( //enqueue a css style sheet
+		'jgnyt-articles-backend-css', //name of the stylesheet (handle)
+		plugins_url( 'jgnyt-articles/jgnyt-articles.css') //name of the src 
+	);
+}
+
+add_action('admin_head', 'jgnyt_articles_backend_styles');
+
+
+?>
